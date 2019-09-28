05178e487969b6163c8ed318fcd102ffe5e5605e
Alberto Laurenzi <35709073+LupodeLupis@users.noreply.github.com>
2019-09-28 19:26:15 -0400

    property data binding in action

--- Databinding/src/app/server-element/server-element.component.ts
+++ Databinding/src/app/server-element/server-element.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
@@ -6,6 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./server-element.component.css']
 })
 export class ServerElementComponent implements OnInit {
+ // @Input() element: {type:string, name:string, content:string};    // Add @Input() decorator for passing the property to to the parent component/any component that host
+                                                                  // <app-server-element></app-server-element>
+
+  @Input('alias') element: {type:string, name:string, content:string};  // if you want use another property name to be exposed outside the child component you can use 'alias' 
+
 
   constructor() { }
 
