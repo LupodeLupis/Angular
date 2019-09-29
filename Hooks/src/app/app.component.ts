a313913aeca38f5732d237d1465d55317e1f9792
Alberto Laurenzi <35709073+LupodeLupis@users.noreply.github.com>
2019-09-29 16:50:20 -0400

    viewChild added

--- Hooks/src/app/app.component.ts
+++ Hooks/src/app/app.component.ts
@@ -29,4 +29,8 @@ export class AppComponent {
    onChangeElement(){
      this.serverElements[0].name = 'Changed!'
    }
+
+   onDestroyFirst(){
+    this.serverElements.splice(0,1); // there is only one element
+  }
 }
--- Hooks/src/app/server-element/server-element.component.ts
+++ Hooks/src/app/server-element/server-element.component.ts
@@ -2,28 +2,72 @@ import { Component,
   OnInit, 
   Input, 
   OnChanges, 
-  SimpleChanges} from '@angular/core';
+  SimpleChanges,
+  DoCheck,
+  AfterContentInit,
+  AfterContentChecked,
+  AfterViewInit,
+  AfterViewChecked,
+  OnDestroy,
+  ViewChild,
+  ElementRef} from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
   templateUrl: './server-element.component.html',
   styleUrls: ['./server-element.component.css']
 })
-export class ServerElementComponent implements OnInit , OnChanges {
+export class ServerElementComponent implements OnInit , 
+OnChanges, 
+DoCheck, 
+AfterContentInit, 
+AfterContentChecked, 
+AfterViewInit, 
+AfterViewChecked, 
+OnDestroy {
   @Input() name: string;
+  @ViewChild('heading', {static:true}) header:ElementRef;
 
-  constructor() { 
+  constructor() {   
     console.log("constructor() is called")
   }
 
   ngOnChanges(changes:SimpleChanges){
     console.log(changes)  // The property name riside inside SimmpleChanges()
-    console.log("ngOnChanges() is called before ngOnInit()")
+    console.log("ngOnChanges() is called")
   } 
-
+  
   ngOnInit() {
     console.log("ngOnInit() is called")
+    console.log(" I cannot access the @VievChild reference " ,"[" ,this.header.nativeElement.textContent, "] inside  ngOnInit() ")
   }
+
+  ngDoCheck(){
+    console.log("ngDoCheck() is called")
+  }
+
+  ngAfterContentInit(){
+    console.log("ngAfterContentInit() is called")
+  }
+
+  ngAfterContentChecked(){
+    console.log("ngAfterContentChecked() is called")
+  }
+
+  ngAfterViewInit(){
+    console.log("ngAfterViewInit() is called")
+    console.log(" .. but I can access it [" ,  this.header.nativeElement.textContent , "] in ngAfterViewInit()")
+  }
+
+  ngAfterViewChecked(){
+    console.log("ngAfterViewChecked() is called")
+  }
+
+  ngOnDestroy(){
+    console.log("ngOnDestroy() is called")
+  }
+
+ 
 
 
   
